fde1077af3d1537c853db049193ca6589e07dd89
eswin.d <eswin.d@thinkpalm.com>
2024-02-08 00:21:08 +0530

    running 4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import Title from './components/Title';
+import Title from "./components/Title";
+import Setup from "./examples/4-global-instance";
 function App() {
   return (
     <main>
       <Title />
+      <Setup />
     </main>
   );
 }
