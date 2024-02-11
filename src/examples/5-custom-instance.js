47095f24ce589b0927a4aa216693e8dc785b9e62
eswin.d <eswin.d@thinkpalm.com>
2024-02-11 23:25:29 +0530

    added

--- src/examples/5-custom-instance.js
+++ src/examples/5-custom-instance.js
@@ -1,16 +1,25 @@
-import { useEffect } from 'react';
+import axios from "axios";
+import { useEffect } from "react";
+import authFetch from "../axios/custom";
 
-const randomUserUrl = 'https://randomuser.me/api';
+const randomUserUrl = "https://randomuser.me/api";
 
 const CustomInstance = () => {
+  console.log(`${authFetch}/api`);
   const fetchData = async () => {
-    console.log('custom axios instance');
+    try {
+      // const resp1 = await axios(`${authFetch.baseURL}/api`);
+      const resp1 = await authFetch("/react-store-products");
+      console.log(resp1);
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>custom instance</h2>;
+  return <h2 className="text-center">custom instance</h2>;
 };
 export default CustomInstance;
